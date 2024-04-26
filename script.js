7275733c07bdb68bed74f7be5fef3ab436ac637f
deeprathod97 <deeprathod9797@gmail.com>
2024-04-26 11:57:12 +0530

    For Loops in javaScript

---
+++ script.js
@@ -0,0 +1,36 @@
+// For Loops in JavaScript
+// Program to add first n natural numbers
+ //initialize the variable with zero value
+
+// Program to add first n natural numbers
+let sum = 0
+let n = prompt("Enter the value of n")
+n = Number.parseInt(n)
+for (let i = 0; i < n; i++) {
+  sum += (i + 1)
+  // console.log((i+1), "+")
+}
+console.log("Sum of first " + n + " natural numbers is " + sum)
+// console.log(i)
+
+
+
+/*
+let obj = {
+  deep: 90,
+  shubh: 45,
+  shivika: 56,
+  ritika: 57,
+  shiv: 23
+}
+
+// For in loop
+for (let a in obj) {
+  console.log("Marks of " + a + " are " + obj[a])
+}
+
+// For of loop
+for (let b of "deep") {
+  console.log(b)
+}
+*/
